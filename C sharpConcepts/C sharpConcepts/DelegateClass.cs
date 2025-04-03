b3ed0d49477559147b720f9e8d84d86a7a20c435
saraswati gowala <saraswatigowala26@gmail.com>
2025-04-03 15:54:58 +0530

    DelegatesExample

---
+++ C sharpConcepts/C sharpConcepts/DelegateClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharpConcepts
+{
+    //Step 1: Define a delegate
+    public delegate void AddDelegates(int a, int b);
+
+    //define a delegate for anonymous method
+    public delegate void PrintMessage(string mssg);
+    public class DelegateClass
+    {
+        public void AddNum(int a, int b)
+        {
+           Console.WriteLine("Adding a number: " + (a + b));
+        }
+
+        public void Multiply(int a, int b)
+        {
+            Console.WriteLine("Multiplying the numbers: " + (a * b));
+        }
+
+    }
+}
---
+++ C sharpConcepts/C sharpConcepts/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Step 2: Instantiate a delegate
+using C_sharpConcepts;
+
+DelegateClass delegateClass = new DelegateClass();
+AddDelegates add = delegateClass.AddNum;
+AddDelegates add2 = delegateClass.Multiply;
+add(3, 7); //calling a delegate method
+add.Invoke(3, 7);
+
+//multicast delegate: using one delegate we can access multiple methods
+//multicast delegate holds the reference of multiple methods4
+add2(2, 3);
+
+
+//anonymous delegates
+PrintMessage Print = delegate (string mssg)
+{
+    Console.Write(mssg);
+};
+
+Print("This is an anonymous delegate");
